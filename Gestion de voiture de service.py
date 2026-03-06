76655182c15b6878bcfa9515af7717b3daaf21e8
LE BON COIN <saidguerda04@gmail.com>
2026-03-06 14:24:00 -0500

    je vienne de créer la méthode affecter_voiture qui permis de effectuer pour un chauffeur une voiture de service si il conntient pas déja une

--- Gestion de voiture de service.py
+++ Gestion de voiture de service.py
@@ -4,3 +4,12 @@ class Employee:
         self.nom=nom
         self.prenom=prenom
         self.voitureService=None
+    def affecter_voiture(self,voiture):
+        if self.voitureService==None:
+            if voiture.chauffeur==None:
+                self.voitureService=voiture
+                voiture.chauffeur=self
+            else:
+                print("la voiture contienne déja un chauffeur")
+        else:
+            print("le chaufeur a déja une voiture de service")
